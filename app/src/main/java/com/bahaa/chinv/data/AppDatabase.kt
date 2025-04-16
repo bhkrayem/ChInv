cd3b6d117359458880f0ca288860f31fa8aeb264
bhkrayem <33270529+bhkrayem@users.noreply.github.com>
2025-04-16 03:02:35 +0300

    Update Gradle settings with migration version
    This commit updates the Gradle settings in the `.idea/gradle.xml` file. It adds a `GradleMigrationSettings` component and sets the `migrationVersion` to "1". This is likely part of a project migration or upgrade process.

--- app/src/main/java/com/bahaa/chinv/data/AppDatabase.kt
+++ app/src/main/java/com/bahaa/chinv/data/AppDatabase.kt
@@ -5,7 +5,7 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 
-@Database(entities = [Item::class, Customer::class], version = 1, exportSchema = false)
+@Database(entities = [Item::class, Customer::class], version = 2, exportSchema = false)
 abstract class AppDatabase : RoomDatabase() {
 
     abstract fun itemDao(): ItemDao
@@ -21,7 +21,9 @@ abstract class AppDatabase : RoomDatabase() {
                     context.applicationContext,
                     AppDatabase::class.java,
                     "chinv_database"
-                ).build()
+                )
+                    .fallbackToDestructiveMigration()
+                    .build()
                 INSTANCE = instance
                 instance
             }
